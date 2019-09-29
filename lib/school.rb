0832ce9b3746f6423f082dee4ec451af79194f65
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-29 23:53:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  
+  def initialize(students_name)
+    @school_name = students_name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @add_student = name
+    @add_student = grade
+    
+    if roster[grade] != nill
+      roster[grade] << name
+      
+     end 
+  end
+  
+  
+end
+
